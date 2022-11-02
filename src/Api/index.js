dbf8bfc762eb98046024b381d7d6942a4bf85266
vanshkr <krvansh30@gmail.com>
2022-11-02 18:25:37 +0530

    deploy change

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,10 @@
  import axios from 'axios';
 
 
- const API = axios.create({ baseURL: 'https://capturememb.herokuapp.com/' });
+//  const API = axios.create({ baseURL: 'https://capturememb.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://capturememb.vercel.app/' });
+// const API = axios.create({ baseURL: 'http://localhost:5000/posts/'});
+
 
  API.interceptors.request.use((req) => {
    if (localStorage.getItem('profile')) {
