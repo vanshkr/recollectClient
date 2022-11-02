1dcf5ece3f66983907baa251e789c68ef414f6ab
vanshkr <krvansh30@gmail.com>
2022-11-02 19:49:02 +0530

    logout issue

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
     dispatch({ type: actionType.LOGOUT });
 
     history.push('/');
-    window.location.reload(true);
+    // window.location.reload(true);
     setUser(null);
   };
   const handleResize = () => {
