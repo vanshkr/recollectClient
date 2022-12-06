1091d441495f328d4fa94b464e23352f980ca927
vanshkr <krvansh30@gmail.com>
2022-12-07 04:55:21 +0530

    hosting change

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,10 +1,9 @@
  import axios from 'axios';
 
 
- const API = axios.create({ baseURL: 'https://capturememb.herokuapp.com/' });
-// const API = axios.create({ baseURL: 'https://capturememb.vercel.app/' });
+//  const API = axios.create({ baseURL: 'https://capturememb.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://recollectserver.onrender.com/' });
 // const API = axios.create({ baseURL: 'http://localhost:5000/posts/'});
-
 
  API.interceptors.request.use((req) => {
    if (localStorage.getItem('profile')) {
